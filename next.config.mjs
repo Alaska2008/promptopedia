eed4da89b4ce08e7d2d2ac93cb41190177e4396c
Alaska2008 <alaskale2024@gmail.com>
2024-03-09 10:16:26 +0000

    Create a build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+ 
+  images: {
+      domains: ['lh3.googleusercontent.com'], // Add your domain(s) here
+    },
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
+};
 
 export default nextConfig;
+
+
